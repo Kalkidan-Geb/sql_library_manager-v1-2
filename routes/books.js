cd7521c42d17a99d033b62ee3d88efd7bb3d0268
Kalkidan-Geb <kalkworku@gmail.com>
2023-11-22 10:53:30 -0800

    update to files

--- routes/books.js
+++ routes/books.js
@@ -2,58 +2,85 @@ const express = require('express');
 const router = express.Router();
 const { Book } = require('../models');
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  };
+}
+
 // GET /books - (displays the full library of books)
-router.get('/', async (req, res, next) => {
+router.get('/', asyncHandler(async (req, res, next) => {
   try {
     const books = await Book.findAll();
     res.render('index', { title: 'Library', books });
   } catch (error) {
     next(error);
   }
-});
+}));
 
 // GET /books/new - (displays the create new book form)
-router.get('/new', (req, res) => {
-  res.render('new-book', { title: 'New Book', book: {} });
+router.get('/new', (req, res, next) => {
+  res.render('new-book', { title: 'New Book', errors: [] });
 });
 
 // POST /books/new - (posts a new book to the database)
-router.post('/new', async (req, res, next) => {
-  const { title, author, genre, year } = req.body;
+router.post('/new', asyncHandler(async (req, res, next) => {
+  const { title, author } = req.body;
+
+  if (!title && !author) {
+    const errors = ['Title and Author are required'];
+    res.render('new-book', { title: 'New Book', errors });
+    return; // Prevent further execution of the route if both fields are empty
+  }
+
+  if (!title) {
+    const errors = ['Title is required'];
+    res.render('new-book', { title: 'New Book', errors });
+    return; // Prevent further execution of the route if title is empty
+  }
+
+  if (!author) {
+    const errors = ['Author is required'];
+    res.render('new-book', { title: 'New Book', errors });
+    return; // Prevent further execution of the route if author is empty
+  }
 
   try {
-    // Use Sequelize model validation
-    const book = await Book.build({ title, author, genre, year });
-    await book.validate();
-    await book.save();
-
+    const book = await Book.create({ title, author });
     res.redirect('/books');
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map((err) => err.message);
-      res.render('new-book', { title: 'New Book', book: { title, author, genre, year, errors } });
+      res.render('new-book', { title: 'New Book', errors });
     } else {
       next(error);
     }
   }
-});
+}));
 
 // GET /books/:id (shows book detail form)
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', asyncHandler(async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
       res.render('update-book', { title: 'Update Book', book });
     } else {
       next();
+      // used to pass to 404 handler
     }
   } catch (error) {
     next(error);
   }
-});
+}));
 
 // POST /books/:id - (updates book information in the database)
-router.post('/:id', async (req, res, next) => {
+router.post('/:id', asyncHandler(async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -61,20 +88,21 @@ router.post('/:id', async (req, res, next) => {
       res.redirect('/books');
     } else {
       next();
+      // also used to pass to 404 handler
     }
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map((err) => err.message);
-      const book = await Book.findByPk(req.params.id);
+      // Do not include errors as a property on book
       res.render('update-book', { title: 'Update Book', book, errors });
     } else {
       next(error);
     }
   }
-});
+}));
 
 // POST /books/:id/delete (used to delete a book)
-router.post('/:id/delete', async (req, res, next) => {
+router.post('/:id/delete', asyncHandler(async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -82,10 +110,11 @@ router.post('/:id/delete', async (req, res, next) => {
       res.redirect('/books');
     } else {
       next();
+      // used to pass to 404 handler again
     }
   } catch (error) {
     next(error);
   }
-});
+}));
 
 module.exports = router;
